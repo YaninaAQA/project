fcf32903ed837b202575f9a818c47f87ae672fbd
Yanina.Matuzava <yaninochka2008@gmail.com>
2023-11-28 21:54:26 +0200

    FirstCommit

--- src/test/java/org/tallin/yanina/StepTest.java
+++ src/test/java/org/tallin/yanina/StepTest.java
@@ -3,57 +3,75 @@ package org.tallin.yanina;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
 
 public class StepTest {
 
 //https://selenide.org/javadoc/current/com/codeborne/selenide/Condition.html
 
-    @Test
-    public void insertingIncorrectCredentials() {
+    @BeforeEach
+    public void setUp() {
+        open("http://51.250.6.164:3000/signin");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--incognito");
         Configuration.browserCapabilities = chromeOptions;
         Configuration.headless = true;
 
-        Selenide.open("http://51.250.6.164:3000/signin");
+    }
+
+    @Test
+    public void insertingIncorrectCredentials() {
+
+
         $x("//input[@data-name=\"username-input\"]").setValue("AnyName");
         $x("//input[@data-name=\"password-input\"]").setValue("AnyPassword");
         $x("//button[@data-name=\"signIn-button\"]").click();
 
         $x("//div[@data-name=\"authorizationError-popup\"]").shouldBe(Condition.exist, Condition.visible);
-
+        $x("//button[@data-name=\"authorizationError-popup-close-button\"]").click();
 
 
     }
 
     @Test
-    public void insertingCorrectCredentials() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--incognito");
-        Configuration.browserCapabilities = chromeOptions;
-        Configuration.headless = true;
+    public void errorCheckingWithOneCharacterInLoginField() {
 
-        Selenide.open("http://51.250.6.164:3000/signin");
-        $x("//input[@data-name=\"username-input\"]").setValue("yaninaaqa");
-        $x("//input[@data-name=\"password-input\"]").setValue("p5Twdy789");
-        $x("//button[@data-name=\"signIn-button\"]").click();
 
-        $x("//button[@data-name=\"createOrder-button\"]").shouldBe(Condition.exist, Condition.visible);
+        $x("//input[@data-name=\"username-input\"]").setValue("W");
+
+
+        $x("//*[@data-name=\"username-input\"]/following-sibling::span[@data-name=\"username-input-error\"]").shouldBe(Condition.exist, Condition.visible);
+
+        $x("//input[@data-name=\"username-input\"]").clear();
+
 
     }
+
+    @Test
+    public void errorCheckingWithOneCharacterInPasswordField() {
+
+
+        $x("//input[@data-name=\"username-input\"]").setValue("yaninaaqa");
+        $x("//input[@data-name=\"password-input\"]").setValue("1");
+
+
+        $x("//*[@data-name=\"password-input\"]/following-sibling::span[@data-name=\"username-input-error\"]").shouldBe(Condition.exist, Condition.visible);
+
+        $x("//input[@data-name=\"password-input\"]").clear();
+
+
+    }
+
 
     @Test
     public void insertingIncorrectCredentialsAndReLogin() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--incognito");
-        Configuration.browserCapabilities = chromeOptions;
-        Configuration.headless = true;
 
-        Selenide.open("http://51.250.6.164:3000/signin");
+
         $x("//input[@data-name=\"username-input\"]").setValue("AnyName");
         $x("//input[@data-name=\"password-input\"]").setValue("AnyPassword");
         $x("//button[@data-name=\"signIn-button\"]").click();
@@ -67,39 +85,23 @@ public class StepTest {
 
         $x("//button[@data-name=\"createOrder-button\"]").shouldBe(Condition.exist, Condition.visible);
 
+        $x("//a[@data-name=\"logout-button\"]").click();
+        $x("//button[@data-name=\"signIn-button\"]").shouldBe(Condition.exist, Condition.visible);
+
 
     }
 
     @Test
-    public void errorCheckingWithOneCharacterInLoginField() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--incognito");
-        Configuration.browserCapabilities = chromeOptions;
-        Configuration.headless = true;
-
-        Selenide.open("http://51.250.6.164:3000/signin");
-        $x("//input[@data-name=\"username-input\"]").setValue("W");
+    public void insertingCorrectCredentials() {
 
 
-        $x("//*[contains(@class, \"form-error form-error_active\")]").shouldBe(Condition.exist, Condition.visible);
-//$x("//*[text()=\"The field must contain  at least of characters: 2\"]").shouldBe(Condition.visible);
-
-    }
-
-    @Test
-    public void errorCheckingWithOneCharacterInPasswordField() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--incognito");
-        Configuration.browserCapabilities = chromeOptions;
-        Configuration.headless = true;
-
-        Selenide.open("http://51.250.6.164:3000/signin");
         $x("//input[@data-name=\"username-input\"]").setValue("yaninaaqa");
-        $x("//input[@data-name=\"password-input\"]").setValue("1");
+        $x("//input[@data-name=\"password-input\"]").setValue("p5Twdy789");
+        $x("//button[@data-name=\"signIn-button\"]").click();
 
+        $x("//button[@data-name=\"createOrder-button\"]").shouldBe(Condition.exist, Condition.visible);
 
-        $x("//*[contains(@class, \"form-error form-error_active\")]").shouldBe(Condition.exist, Condition.visible);
-
+        $x("//a[@data-name=\"logout-button\"]").click();
 
     }
 
