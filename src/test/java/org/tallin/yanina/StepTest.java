cc0f0c2115da4ce5b92035fe171f419aa40035d7
Yanina.Matuzava <yaninochka2008@gmail.com>
2023-12-18 19:33:28 +0200

    Revert

--- src/test/java/org/tallin/yanina/StepTest.java
+++ src/test/java/org/tallin/yanina/StepTest.java
@@ -15,18 +15,11 @@ public class StepTest {
 
     @BeforeEach
     public void setUp() {
-        Configuration.browser = "chrome"; // or whatever browser you are using
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+        Configuration.browserCapabilities = chromeOptions;
         Configuration.headless = true;
-        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions());
         open("http://51.250.6.164:3000/signin");
-    }
-
-    private static ChromeOptions chromeOptions() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-gpu");
-        return options;
     }
 
     @Test
